8149e4e79166e2fcc4f75baf3e29bfd446120ef6
Faahim Sadman <57841708+SadManFahIm@users.noreply.github.com>
2021-06-15 18:10:27 +0600

    Own Commit

---
+++ 1113.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ 
+int main(){
+    int x,y;
+    while(scanf("%d%d",&x,&y)&&x!=y){
+    if(x>y){
+        printf("Decrescente\n");
+    }
+    else{
+        printf("Crescente\n");
+    }
+    }
+    return 0;//end line
+}
