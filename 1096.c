053f88a164f4af6868677c0f77e1c68e1db9c582
Faahim Sadman <57841708+SadManFahIm@users.noreply.github.com>
2021-06-15 18:03:09 +0600

    Commited

---
+++ 1096.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int i,j;
+ 
+    for(i=1,j=7;j>=5;j--){
+        printf("I=%d J=%d\n",i,j);
+    }
+    for(i=3,j=7;j>=5;j--){
+        printf("I=%d J=%d\n",i,j);
+    }
+    for(i=5,j=7;j>=5;j--){
+        printf("I=%d J=%d\n",i,j);
+    }
+    for(i=7,j=7;j>=5;j--){
+        printf("I=%d J=%d\n",i,j);
+    }
+    for(i=9,j=7;j>=5;j--){
+        printf("I=%d J=%d\n",i,j);
+    }
+    return 0;//end line
+}
