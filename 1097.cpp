c91ff8e5f9e9efe880f7fbd3d1bddf439ee65247
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-01-15 00:23:14 +0600

    Create 1097.cpp

---
+++ 1097.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i, j, k;
+    
+    for(i = 1; i <= 9; i++){
+        j = i + 6;
+        for(k = 0; k < 3; k++){
+            cout << 
+    }
+
+    return 0;
+}
