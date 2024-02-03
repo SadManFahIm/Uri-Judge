4b8157cc1ff1401feb44d6ddc40bdd031ad52cd4
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-02-04 00:38:01 +0600

    Updated

---
+++ 1144.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n, k, l, i;
+
+    cin >> n;
+
+    k = 1;
+    l = 1;
+    for(i = 1; i <= n; i++){
+        k = i * i;
+        l = k * i;
+
+        cout << i << ' ' << k << ' ' << l << '\n';
+        cout << i << ' ' << k+1 << ' ' << l+1 << '\n';
+    }
+
+
+    return 0;
+}
