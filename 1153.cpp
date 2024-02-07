3eed60db89326b4ea7e969d8fa26f12310ea52d3
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-02-07 07:51:17 +0600

    updated

---
+++ 1153.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, fatorial, i;
+
+    cin >> n;
+
+    fatorial = n;
+
+    for(i = n-1; i >= 1; i--)
+        fatorial *= i;
+
+    cout << fatorial << '\n';
+    
+    return 0;
+}
