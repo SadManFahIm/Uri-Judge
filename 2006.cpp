4ae7bf32cbbc505a911d198042e211a8cc38b002
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-05-16 00:13:27 +0600

    Create 2006.cpp

---
+++ 2006.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t, vet[5] , acertos = 0;
+
+    cin >> t;
+    for(int i = 0; i < 5; i++){
+        cin >> vet[i];
+        if(vet[i] == t)
+            acertos++;
+    }
+
+    cout << acertos << endl;
+    
+    return 0;
+}
