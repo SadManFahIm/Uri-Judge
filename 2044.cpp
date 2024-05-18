a13242031fc49ad0445b8cbc942b6ca26f29759e
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-05-18 18:52:03 +0600

    Create 2044.cpp

---
+++ 2044.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, acumulado, visitas, p;
+
+    while(true){
+        cin >> n;
+
+        if(n == -1)
+            break;
+
+        acumulado = 0;
+        visitas = 0;
+
+        for(int i = 0; i < n; i++){
+            cin >> p;
+            acumulado += p;
+
+            if(acumulado % 100 == 0){
+                visitas++;
+                acumulado = 0;
+            }
+        }
+
+        cout << visitas << "\n";
+    }
+}
