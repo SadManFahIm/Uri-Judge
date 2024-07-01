79735ef4a2e37158044fef0a9c622c3f3c3c2800
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-07-02 01:13:19 +0600

    Create 2581.cpp

---
+++ 2581.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    while(n--){
+        cin.ignore();
+        cout << "I am Toorg!\n";
+    }
+
+    return 0;
+}
