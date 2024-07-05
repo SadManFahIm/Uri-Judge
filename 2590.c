1af3b543441b2eb5423bb6cf5047f91cf1a6f0cf
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-07-05 21:57:03 +0600

    Create 2590.c

---
+++ 2590.c
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t, r;
+    long int n;
+
+    /*
+    Padrão:
+    (7)
+    4(9)
+    34(3)
+    240(1)
+    1680(7)
+    11764(9)
+    82354(3)
+    576480(1)
+    4035360(7)
+
+    O final da potência sempre seguirá a sequência 7 - 9 - 3 - 1
+    */
+
+    scanf("%d", &t);
+    while(t > 0){
+        scanf("%li", &n);
+
+        r = n % 4;
+        switch(r){
+            case 0:
+                printf("1\n");
+                break;
+            case 1:
+                printf("7\n");
+                break;
+            case 2:
+                printf("9\n");
+                break;
+            case 3:
+                printf("3\n");
+                break;
+        }
+
+        t--;
+    }
+
+    return 0;
+}
