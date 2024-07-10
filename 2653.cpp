71de0f1c8295b7b222e9ad6de815cb32a0800e8f
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-07-10 23:17:04 +0600

    Create 2653.cpp

---
+++ 2653.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int tipos = 0;
+    string joia;
+    set<string> banco;
+
+    while(cin >> joia){
+        if(banco.find(joia) == banco.end()){
+            banco.insert(joia);
+            tipos++
+        }
+    }
+
+    cout << tipos << endl;
+    return 0;
+}
