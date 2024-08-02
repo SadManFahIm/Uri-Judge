85a6381b28ba1c5e1cd812e3113f4b68be784c4a
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-08-02 14:15:49 +0600

    Create 2864.c

---
+++ 2864.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int t, a, b, c, delta;
+    double yMax;
+
+    scanf("%d", &t);
+
+    while(t--){
+        scanf("%d %d %d", &a, &b, &c);
+
+        delta = pow(b, 2) - 4 * a * c;
+        yMax = -((double)delta / (double)(4 * a));
+
+        printf("%.2lf\n", yMax);
+    }
+
+    return 0;
+}
