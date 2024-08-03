4ba3d91d003ca17cb79061206d85c022597f1f90
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-08-03 14:07:15 +0600

    Create 2864.cpp

---
+++ 2864.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int t, a, b, c, delta;
+    double yMax;
+
+    cin >> t;
+
+    cout << fixed << setprecision(2);
+    while(t--){
+        cin >> a >> b >> c;
+
+        delta = pow(b, 2) - 4 * a * c;
+        yMax = -(double(delta) / double(4 * a));
+
+        cout << yMax << endl; 
+    }
+
+    return 0;
+}
