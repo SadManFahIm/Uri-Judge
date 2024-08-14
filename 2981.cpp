3f68e83692063106b8e7c2e1f4cc2d68b661473e
Sadman Fahim <57841708+SadManFahIm@users.noreply.github.com>
2024-08-15 00:24:42 +0600

    Create 2981.cpp

---
+++ 2981.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int c, g;
+    
+    cin >> c >> g;
+    if ((int)(c / g) < 10)
+        cout << "A UFSC fecha dia " << (int)(c / g) + 21 << " de setembro." << endl;
+    else
+        cout << "A UFSC fecha dia " << (int)(c / g) - 9 << " de outubro." << endl;
+    
+    return 0;
+}
